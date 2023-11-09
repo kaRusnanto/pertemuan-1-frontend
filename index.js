605d46bec7e361cdac857bf19c967a5d20b42ef0
Ka rusnanto <Karusnanto@gmail.com>
2023-11-09 11:00:10 +0700

    pertemuan-1

---
+++ index.js
@@ -0,0 +1,27 @@
+
+// minimalist.pop
+let number = [1, 2, 3,];
+let removednumber = number.pop(); 
+console.log(number); 
+console.log(removednumber);
+document.write(number) 
+
+// minimalist.push
+let fruits = ["apple", "banana"];
+fruits.push("cherry", "watermelon"); 
+console.log(fruits); 
+document.write(fruits)
+
+// minimalist.shift
+let bers = ["apple", "banana", "cherry"];
+bers.shift();
+console.log(bers);
+document.write(bers);
+
+// minimalist.unshift
+let buah = ["banana", "cherry"];
+buah.unshift("apple", "date");
+console.log(buah);
+document.write(buah);
+
+
